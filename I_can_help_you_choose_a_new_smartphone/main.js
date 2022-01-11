e5fa7ab69bdf40c07844ebd2acc5a8bffe95bbc8
vitalii-minchuk <mcmin4doutsch@gmail.com>
2022-01-12 01:25:36 +0200

    function creating random objects implemented

--- I_can_help_you_choose_a_new_smartphone/main.js
+++ I_can_help_you_choose_a_new_smartphone/main.js
@@ -1,2 +1,32 @@
 
-console.log('hello');
+function makeItems() {
+
+    const makes = ['Apple', 'Huawei', 'Motorola', 'Nokia', 'Samsung', 'ZTE', 'Xiaomi'];
+    const price = getRandomPrice(1000, 30000);;
+    const freeDelivery =[false, true];
+
+    let randomMakes = Math.floor(Math.random() * makes.length);
+    let randomDelivery = Math.floor( Math.random() * 2);
+
+    function getRandomPrice(min, max) {
+
+        let num = Math.random() * (max - min) + min;
+
+            beautifullNum = Math.trunc(num / 100) * 100;
+    return beautifullNum;
+    }
+
+    let item = {
+        make: makes[randomMakes],
+        price,
+        'free delivery': freeDelivery[randomDelivery],
+    };
+    return item;
+}
+let iter = 5;
+i = 0;
+while (i < iter) {
+    makeItems();
+    console.log(makeItems());
+    i++;
+}
